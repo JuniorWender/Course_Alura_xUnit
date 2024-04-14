c229054d3ad08cd4242e619f0f76be6c6d79d550
Junior Wender <juniorwender1996@gmail.com>
2024-04-14 20:16:04 -0300

    Create Test

---
+++ tests/JornadaMilhas.Test/GerenciadorDeOfertasRecuperaMaiorDesconto.cs
@@ -0,0 +1,75 @@
+﻿using Bogus;
+using JornadaMilhasV1.Gerencidor;
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class GerenciadorDeOfertasRecuperaMaiorDesconto
+    {
+        [Fact]
+        public void RetornaOfertaNulaQuandoListaEstaVazia()
+        {
+            //Arrange
+            List<OfertaViagem> lista = new List<OfertaViagem>();
+            GerenciadorDeOfertas gerenciador = new GerenciadorDeOfertas(lista);
+            Func<OfertaViagem, bool> filtro = o => o.Rota.Destino.Equals("São Paulo");
+
+            // Act
+            var oferta = gerenciador.RecuperaMaiorDesconto(filtro);
+
+            //Assert
+            Assert.Null(oferta);
+        }
+
+        [Fact]
+        // destino = São Paulo, desconto = 40, preco = 80
+        public void RetornaOfertaEspecificaQuandoDestinoSaoPauloEDesconto40()
+        {
+            //Arrange
+            Faker<Periodo> fakerPeriodo = new Faker<Periodo>()
+                .CustomInstantiator(f =>
+                {
+                    DateTime dataInicio = f.Date.Soon();
+                    return new Periodo(dataInicio, dataInicio.AddDays(30));
+                });
+
+            Rota rota = new Rota("Vitoria", "São Paulo");
+
+            Faker<OfertaViagem> fakerOferta = new Faker<OfertaViagem>()
+                .CustomInstantiator(f => new OfertaViagem(
+                    rota,
+                    fakerPeriodo.Generate(),
+                    100 * f.Random.Int(1, 100))
+                )
+                .RuleFor(o => o.Desconto, f => 40)
+                .RuleFor(o => o.Ativa, f => true);
+
+            OfertaViagem ofertaEscolhida = new OfertaViagem(rota, fakerPeriodo.Generate(), 80)
+            {
+                Desconto = 40,
+                Ativa = true
+            };
+
+            OfertaViagem ofertaInativa = new OfertaViagem(rota, fakerPeriodo.Generate(), 70)
+            {
+                Desconto = 40,
+                Ativa = false
+            };
+
+            List<OfertaViagem> lista = fakerOferta.Generate(200);
+            lista.Add(ofertaEscolhida);
+            lista.Add(ofertaInativa);
+
+            GerenciadorDeOfertas gerenciador = new GerenciadorDeOfertas(lista);
+            Func<OfertaViagem, bool> filtro = o => o.Rota.Destino.Equals("São Paulo");
+            double precoEsperado = 40.00;
+
+            // Act
+            var oferta = gerenciador.RecuperaMaiorDesconto(filtro);
+
+            //Assert
+            Assert.NotNull(oferta);
+            Assert.Equal(precoEsperado, oferta.Preco, 0.0001);
+        }
+    }
+}
---
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,59 @@
+﻿using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemDesconto
+    {
+        [Fact]
+        public void RetornaPrecoAtualizadoQuandoAplicadoDesconto()
+        {
+            // Arrange
+            Rota rota = new Rota("OrigemA", "DestinoB");
+            Periodo periodo = new Periodo(new DateTime(2024,05,01), new DateTime(2024,05,10));
+            double precoOriginal = 100;
+            double desconto = 20.00;
+            double precoComDesconto = precoOriginal - desconto;
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+            //Act
+            oferta.Desconto = desconto;
+
+            //Assert
+            Assert.Equal(precoComDesconto, oferta.Preco);
+        }
+
+        [Theory]
+        [InlineData(120, 30)]
+        [InlineData(100, 30)]
+        public void RetornaDescontoMaximoQuandoValorDescontoMaiorOuIgualAoPreco(double desconto, double precoComDesconto)
+        {
+            // Arrange
+            Rota rota = new Rota("OrigemA", "DestinoB");
+            Periodo periodo = new Periodo(new DateTime(2024, 05, 01), new DateTime(2024, 05, 10));
+            double precoOriginal = 100;
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+            //Act
+            oferta.Desconto = desconto;
+
+            //Assert
+            Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
+        }
+
+        [Fact]
+        public void RetornaTresErrosDeValidacaoQuandoRotaPeriodoEPrecoSaoInvalidos()
+        {
+            // Arrange
+            int quantidadeEsperada = 3;
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 6, 1), new DateTime(2024, 5, 10));
+            double preco = -100;
+
+            //Act
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            //Assert
+            Assert.Equal(quantidadeEsperada, oferta.Erros.Count());
+        }
+    }
+}
